7cb02c8a8e4477f1b1a47e8c6090a0428f459430
unknown <labibmahodi.bd@gmail.com>
2023-10-31 12:41:02 +0600

    menus

--- src/pages/Menu/Menu/Menu.jsx
+++ src/pages/Menu/Menu/Menu.jsx
@@ -5,6 +5,9 @@ import useMenu from '../../../hooks/useMenu';
 import SectionTitles from '../../../Components/SectionTitles/SectionTitles';
 import MenuCategory from '../MenuCategory/MenuCategory';
 import dessertImg from '../../../assets/menu/dessert-bg.jpeg'
+import pizzaImg from '../../../assets/menu/pizza-bg.jpg'
+import saladImg from '../../../assets/menu/salad-bg.jpg'
+import soupImg from '../../../assets/menu/soup-bg.jpg'
 
 const Menu = () => {
 
@@ -27,7 +30,16 @@ const Menu = () => {
             <MenuCategory items={offered}></MenuCategory>
 
             {/* dessert */}
-            <MenuCategory items={dessert} title={'Dessert'} img={dessertImg}></MenuCategory>
+            <MenuCategory items={dessert} title={'Desserts'} img={dessertImg}></MenuCategory>
+            
+            {/* pizza */}
+            <MenuCategory items={pizza} title={'Pizzas'} img={pizzaImg}></MenuCategory>
+            
+            {/* salad */}
+            <MenuCategory items={salad} title={'Salads'} img={saladImg}></MenuCategory>
+            
+            {/* soup */}
+            <MenuCategory items={soup} title={'Soups'} img={soupImg}></MenuCategory>
 
         </div>
     );
